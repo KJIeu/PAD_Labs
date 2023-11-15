3020b46bdca8b3bcab35c729133069c1f9dbf9e5
Alexandr Boico <65282522+KJIeu@users.noreply.github.com>
2023-11-15 21:22:10 +0200

    Add files via upload

---
+++ product-service/src/main/java/com/pad/productservice/repository/ProductRepository.java
@@ -0,0 +1,8 @@
+package com.pad.productservice.repository;
+
+
+import com.pad.productservice.model.Product;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface ProductRepository extends MongoRepository<Product, String> {
+}
