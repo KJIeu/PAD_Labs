001ea8aaa3cb1e6fadfdac99e0f2f1739e00ac1f
Alexandr Boico <65282522+KJIeu@users.noreply.github.com>
2023-11-23 14:30:54 +0200

    Add files via upload

--- inventory-service/src/main/java/com/pad/inventoryservice/controller/InventoryController.java
+++ inventory-service/src/main/java/com/pad/inventoryservice/controller/InventoryController.java
@@ -1,10 +1,13 @@
 package com.pad.inventoryservice.controller;
 
 
+import com.pad.inventoryservice.dto.InventoryResponse;
 import com.pad.inventoryservice.service.InventoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/inventory")
@@ -13,9 +16,9 @@ public class InventoryController {
 
     private final InventoryService inventoryService;
 
-    @GetMapping("/{sku-code}")
+    @GetMapping
     @ResponseStatus(HttpStatus.OK)
-    public boolean isInStock(@PathVariable("sku-code") String skuCode){
+    public List<InventoryResponse> isInStock(@RequestParam List<String> skuCode){
         return inventoryService.isInStock(skuCode);
     }
 }
--- order-service/src/main/java/com/pad/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/pad/orderservice/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.pad.orderservice.service;
 
+import com.pad.orderservice.dto.InventoryResponse;
 import com.pad.orderservice.model.Order;
 import com.pad.orderservice.model.OrderLineItems;
 import com.pad.orderservice.dto.OrderLineItemsDto;
@@ -7,16 +8,21 @@ import com.pad.orderservice.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 import com.pad.orderservice.dto.OrderRequest;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.reactive.function.client.WebClient;
 
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class OrderService {
 
     private final OrderRepository orderRepository;
+    private final WebClient webClient;
 
     public void placeOrder(OrderRequest orderRequest){
         Order order = new Order();
@@ -29,7 +35,22 @@ public class OrderService {
 
         order.setOrderLineItemsList(orderLineItems);
 
-        orderRepository.save(order);
+        List<String> skuCodes = order.getOrderLineItemsList().stream().map(OrderLineItems::getSkuCode).toList();
+        // Call Inventory service, and place order if product is in stock
+        InventoryResponse[] inventoryResponses = webClient.get()
+                .uri("http://localhost:8082/api/inventory",
+                        uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
+                .retrieve()
+                .bodyToMono(InventoryResponse[].class).block();
+
+        boolean allProductsInStock = Arrays.stream(inventoryResponses).allMatch(InventoryResponse::isInStock);
+
+        if (allProductsInStock) {
+            orderRepository.save(order);
+        } else {
+            throw new IllegalArgumentException("Product is not in stock");
+        }
+
     }
 
 
