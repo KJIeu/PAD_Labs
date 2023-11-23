9fb130feaaa8f1db1dfb6ea78b513cf458c76dce
Alexandr Boico <65282522+KJIeu@users.noreply.github.com>
2023-11-23 03:11:30 +0200

    Add files via upload

---
+++ inventory-service/src/main/java/com/pad/inventoryservice/InventoryServiceApplication.java
@@ -0,0 +1,37 @@
+package com.pad.inventoryservice;
+
+import com.pad.inventoryservice.model.Inventory;
+import com.pad.inventoryservice.repository.InventoryRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class InventoryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InventoryServiceApplication.class, args);
+    }
+
+    /*@Bean
+    public CommandLineRunner loadData(InventoryRepository inventoryRepository){
+        return args -> {
+            Inventory inventory = new Inventory();
+            inventory.setSkuCode("iphone_13");
+            inventory.setQuantity(10);
+
+            Inventory inventory1 = new Inventory();
+            inventory1.setSkuCode("iphone_13_red");
+            inventory1.setQuantity(0);
+
+            Inventory inventory2 = new Inventory();
+            inventory2.setSkuCode("krotovuha");
+            inventory2.setQuantity(20);
+
+            inventoryRepository.save(inventory);
+            inventoryRepository.save(inventory1);
+            inventoryRepository.save(inventory2);
+        };
+    }*/
+}
